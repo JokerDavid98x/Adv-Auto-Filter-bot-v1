278debd0cfdba09c1ab524542b2449b9fb848e6d
JokerDavid98x <87626569+JokerDavid98x@users.noreply.github.com>
2021-09-11 08:58:16 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} /n @OnRockers_Official1",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕️OUR CHANNEL⭕️', url="https://t.me/OnRockers_Official1"
                                 )
                         ]
                     ]
